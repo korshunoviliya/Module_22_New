b0895aa74d2c14163c575f1592a29de6208e2574
korshunov80@mail.ru <korshunov80@mail.ru>
2021-10-21 19:00:48 +0300

    Update module_22_3.cpp
    task 3 is ready
    added check for the number of repeated letters

--- module_22_3.cpp
+++ module_22_3.cpp
@@ -5,29 +5,48 @@
 bool anagramm(std::string first, std::string second)
 {
     if(first.length() != second.length()) return false;
+
     std::map<char, int> firstWord;
     std::map<char, int> secondWord;
     auto itFirst = firstWord.begin();
     auto itSecond = secondWord.begin();
+    int count{0};
 
     for(int i = 0; i < first.length(); i++)
     {
-        firstWord.insert(std::pair<char, int> (first[i], i));
-        secondWord.insert(std::pair<char, int> (second[i], i));
+        count = 0;
+        for(int j = i; j < first.length(); j++)
+        {
+            if(first[i] == first[j]) count++;
+        }
+        firstWord.insert(std::pair<char, int> (first[i], count));
+
+        count = 0;
+        for(int j = i; j < second.length(); j++)
+        {
+            if(second[i] == second[j]) count++;
+        }
+        secondWord.insert(std::pair<char, int> (second[i], count));
     }
 
-   for(itFirst = firstWord.begin(), itSecond = secondWord.begin();
+    for(itFirst = firstWord.begin(), itSecond = secondWord.begin();
             itFirst != firstWord.end() || itSecond != secondWord.end();
             itFirst++, itSecond++)
     {
-        if(itFirst -> first != itSecond -> first) return false;
+        if((itFirst -> first != itSecond -> first)
+                || (itFirst -> second != itSecond -> second)) return false;
     }
     return true;
 }
 
 int main() {
 
-    if(anagramm("стекловар", "лекарство")) std::cout << "ok!" << std::endl;
+    //bool check = anagramm("hello", "heloo");
+    //bool check = anagramm("steklovar", "lekarstvo");
+    //bool check = anagramm("hello", "lhole");
+    bool check = anagramm("СЃС‚РµРєР»РѕРІР°СЂ", "Р»РµРєР°СЂСЃС‚РІРѕ");
+
+    if(check) std::cout << "ok!" << std::endl;
     else std::cout << "no!" << std::endl;
     return 0;
 }
